a8f6bcd02095cc13b6839615b9a4c9a488b2de16
mie998 <nishiwaki.kyoto@gmail.com>
2021-06-16 23:37:07 +0900

    added some queryselectors

--- src/index.js
+++ src/index.js
@@ -1,29 +1,25 @@
-window.addEventListener("load", main, false);
-
 const main = () => {
-    const jsInitCheckTimer = setInterval(jsLoaded, 1000);
     const jsLoaded = () => {
-        const superChatTicketRenderer = document.getElementsByClassName(
-            "yt-live-chat-ticker-renderer"
+        const superChatTicker = document.getElementById("ticker");
+        const superChatTicketsContainer = document.querySelector(
+            ".style-scope.yt-live-chat-ticker-renderer"
         );
+        console.log(superChatTicketsContainer);
         const liveChatSuperChatRenderer = document.getElementsByClassName(
-            "yt-live-chat-item-list-renderer"
+            "style-scope yt-live-chat-item-list-renderer"
         );
+        const test = document.querySelector(".ytd-video-owner-renderer");
         const chipBadges = document.getElementById("chip-badges");
         const member = document.getElementsByClassName("member");
         const paidMessages = document.getElementsByClassName(
             "yt-live-chat-paid-message-renderer"
         );
 
-        const elementDiscards = [
-            superChatTicketRenderer,
-            liveChatSuperChatRenderer,
-            chipBadges,
-        ];
-        const StyleChanges = [member, paidMessages];
-
-        superChatTicketRenderer.classList.remove(
-            "yt-live-chat-ticker-renderer"
-        );
+        superChatTicker.style.display = "none !important";
+        superChatTicketsContainer.style.display = "none !important";
+        test.style.display = "none";
     };
+    setTimeout(jsLoaded, 300);
 };
+
+window.addEventListener("load", main, false);
